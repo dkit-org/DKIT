5c7388ed1fdb8383817025c9d37f885be2a7f7e5
moamenhredeen <moamenhredeen@gmail.com>
2022-06-12 20:46:29 +0200

    cache the views when loading the fxml

--- client/src/main/java/org/dkit/fxclient/common/ViewLoader.java
+++ client/src/main/java/org/dkit/fxclient/common/ViewLoader.java
@@ -1,4 +1,4 @@
-package org.dkit.fxclient.controller;
+package org.dkit.fxclient.common;
 
 import javafx.fxml.FXMLLoader;
 import org.springframework.context.ApplicationContext;
@@ -6,17 +6,25 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class ViewLoader {
 
     private final ApplicationContext applicationContext;
+    public Map<String, Object> views = new HashMap<>();
 
     public ViewLoader(ApplicationContext applicationContext) {
         this.applicationContext = applicationContext;
     }
 
     public <T> T load(final String path){
+        // check if view loaded
+        if(views.containsKey(path)) {
+            return (T) this.views.get(path);
+        }
+
         try {
             var url = new ClassPathResource(path).getURL();
             var loader = new FXMLLoader(url);
